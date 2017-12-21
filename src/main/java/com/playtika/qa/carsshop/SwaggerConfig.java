ec98a97fd5b7b6bb796ad281c66f4cdf00568290
vbazylevych <vbazylevych@playtika.com>
2017-12-21 19:04:24 +0200

    swagger

--- src/main/java/com/playtika/qa/carsshop/SwaggerConfig.java
+++ src/main/java/com/playtika/qa/carsshop/SwaggerConfig.java
@@ -27,7 +27,7 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Car Service")
+                .title("Car shop API")
                 .version("1.0").build();
     }
 }
--- src/main/java/com/playtika/qa/carsshop/web/CarController.java
+++ src/main/java/com/playtika/qa/carsshop/web/CarController.java
@@ -4,6 +4,8 @@ import com.playtika.qa.carsshop.domain.Car;
 import com.playtika.qa.carsshop.domain.CarInStore;
 import com.playtika.qa.carsshop.domain.CarInfo;
 import com.playtika.qa.carsshop.service.CarService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -15,11 +17,12 @@ import java.util.Collection;
 @Slf4j
 @RestController
 @AllArgsConstructor
+@Api(description = "Operations pertaining to products in Online Store")
 public class CarController {
 
     private final CarService service;
 
-
+    @ApiOperation("Opens new ads")
     @PostMapping(value = "/cars", produces = MediaType.APPLICATION_JSON_VALUE)
     public long createCar(@RequestParam("price") int price,
                           @RequestParam("contact") String contactDetails,
@@ -27,10 +30,11 @@ public class CarController {
         log.info("Create new car request was received");
 
         CarInStore carInStore = new CarInStore(car, new CarInfo(price, contactDetails));
-       return service.add(carInStore).getCar().getId();
+        return service.add(carInStore).getCar().getId();
 
     }
 
+    @ApiOperation("Returns info about specified car")
     @GetMapping(value = "/cars/{id}")
     public CarInfo getCar(@PathVariable(value = "id") long id) throws NotFoundException {
         log.info("get request with id {} received", id);
@@ -40,12 +44,14 @@ public class CarController {
                 .orElseThrow(() -> new NotFoundException("Can't find car info"));
     }
 
+    @ApiOperation("Returns all opened ads")
     @GetMapping("/cars")
     public Collection<CarInStore> getAllCars() {
         log.info("Get all cars request was received");
         return service.getAll();
     }
 
+    @ApiOperation("Deletes car and related ads")
     @DeleteMapping("cars/{id}")
     public void deleteCars(@PathVariable("id") long id) {
         service.delete(id);

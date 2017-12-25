1ef0515976c819d69421f1d2b14eb410dfc90e3f
vbazylevych <608levlerka>
2017-12-25 11:46:46 +0200

    а

--- src/main/java/com/playtika/qa/carsshop/domain/Car.java
+++ src/main/java/com/playtika/qa/carsshop/domain/Car.java
@@ -1,5 +1,6 @@
 package com.playtika.qa.carsshop.domain;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 
@@ -7,9 +8,13 @@ import lombok.*;
 @AllArgsConstructor
 public class Car {
     private long id;
+    @ApiModelProperty(required = true)
     private final String plateNumber;
+    @ApiModelProperty(required = true)
     private String model = "";
+    @ApiModelProperty(required = true)
     private String color = "";
+    @ApiModelProperty(required = true)
     private int year = 1901;
 
     public Car(String plateNumber) {
--- src/main/java/com/playtika/qa/carsshop/web/CarController.java
+++ src/main/java/com/playtika/qa/carsshop/web/CarController.java
@@ -6,6 +6,8 @@ import com.playtika.qa.carsshop.domain.CarInfo;
 import com.playtika.qa.carsshop.service.CarService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -23,6 +25,10 @@ public class CarController {
     private final CarService service;
 
     @ApiOperation("Opens new ads")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully opened ads"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 500, message = "Car already selling")})
     @PostMapping(value = "/cars", produces = MediaType.APPLICATION_JSON_VALUE)
     public long createCar(@RequestParam("price") int price,
                           @RequestParam("contact") String contactDetails,
@@ -35,6 +41,10 @@ public class CarController {
     }
 
     @ApiOperation("Returns info about specified car")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully returned ads info"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 404, message = "Car not found")})
     @GetMapping(value = "/cars/{id}")
     public CarInfo getCar(@PathVariable(value = "id") long id) throws NotFoundException {
         log.info("get request with id {} received", id);
@@ -45,6 +55,9 @@ public class CarController {
     }
 
     @ApiOperation("Returns all opened ads")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully returned ads"),
+            @ApiResponse(code = 400, message = "Bad request")})
     @GetMapping("/cars")
     public Collection<CarInStore> getAllCars() {
         log.info("Get all cars request was received");
@@ -52,6 +65,9 @@ public class CarController {
     }
 
     @ApiOperation("Deletes car and related ads")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully deleted car and related info"),
+            @ApiResponse(code = 400, message = "Bad request")})
     @DeleteMapping("cars/{id}")
     public void deleteCars(@PathVariable("id") long id) {
         service.delete(id);

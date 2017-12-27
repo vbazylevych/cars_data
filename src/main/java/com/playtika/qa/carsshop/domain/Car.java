e88ff47441e005dad3d47caed43f4c001028f8fb
vbazylevych <608levlerka>
2017-12-27 09:52:39 +0200

    а

--- src/main/java/com/playtika/qa/carsshop/domain/Car.java
+++ src/main/java/com/playtika/qa/carsshop/domain/Car.java
@@ -7,14 +7,15 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 public class Car {
+    @ApiModelProperty(hidden = true)
     private long id;
-    @ApiModelProperty(required = true)
+    @ApiModelProperty(required = true, example = "xxx")
     private final String plateNumber;
-    @ApiModelProperty(required = true)
+    @ApiModelProperty(required = true, example = "opel")
     private String model = "";
-    @ApiModelProperty(required = true)
+    @ApiModelProperty(required = true, example = "red")
     private String color = "";
-    @ApiModelProperty(required = true)
+    @ApiModelProperty(required = true, value = ">1900", example = "2000")
     private int year = 1901;
 
     public Car(String plateNumber) {
